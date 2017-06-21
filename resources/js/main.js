4711a241ca42901bddbe1a7a601f707f53aafe9d
skramerinco <kramer_gs@hotmail.com>
2017-06-21 13:32:36 -0600

    Final

---
+++ resources/js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+   $('.carousel').slick({
+     autoplay: true,
+     autoplaySpeed: 4000,
+     arrows: false,
+     pauseOnHover: false,
+     pauseOnFocus: false
+   });
+
+
+   
+ });
